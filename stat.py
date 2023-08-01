2e341c186d0b99ae5f257d00d9fc488fff8dd083
Alex Everett <140557166+aeverettTGK@users.noreply.github.com>
2023-08-01 17:17:52 -0600

    Rewrite stat.py
    Essentially a file upload as everything was rewritten from scratch
    
    Functions to create dictionary from fasta file, position objects for each position on the sequence(s), and takes statistics from position objects and places them into a csv file for future use in plots

--- stat.py
+++ stat.py
@@ -1,13 +1,49 @@
 from position import Position
+from Bio import SeqIO
+import csv
 
-aa_dict = {}
+def read_fasta_file(file_path):
+    sequence_dict = {}
+    for record in SeqIO.parse(file_path, "fasta"):
+        sequence_dict[record.id] = str(record.seq)
+    return sequence_dict
 
-with open("enterobacteralesOA.fa", "r") as fasta_file:
-            read_file = fasta_file.read()
-            lines = read_file.split(">")
-            for line in lines:
-                temp = line.split("\n")
-                print(temp[0])
-                print(temp[1])
+def create_sites_list(seq_dict):
+    sites = []
+    for pos_num in range(len(list(seq_dict.values())[0])):
+        position = Position(pos_num, seq_dict)
+        sites.append(position)
+    return sites
+
+def get_proportions(sites):
+    proportions = []
+    for pos_obj in sites:
+        prop = pos_obj.proportion()
+        proportions.append(prop)
+    return proportions
+
+def get_unique_aa(sites):
+    unique_list = []
+    for pos_obj in sites:
+        unique_count = pos_obj.unique()
+        unique_list.append(unique_count)
+    return unique_list
+
+def write_to_csv(filename, positions, proportions, unique_counts):
+    with open(filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Position', 'Proportion', 'Unique Amino Acids'])
+        for pos, proportion, unique_count in zip(positions, proportions, unique_counts):
+            csv_writer.writerow([pos + 1, proportion, unique_count])
+
+if __name__ == "__main__":
+    fasta_file = "enterobacteralesOA.fa"
+    seq_dict = read_fasta_file(fasta_file)
+    sites_list = create_sites_list(seq_dict)
+    positions = list(range(len(list(seq_dict.values())[0])))  
+    proportions_list = get_proportions(sites_list)
+    unique_counts_list = get_unique_aa(sites_list)
+    output_file = "entero_stats.csv"
+    write_to_csv(output_file, positions, proportions_list, unique_counts_list)
 
 
