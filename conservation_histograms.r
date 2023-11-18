a5889771e4439766d709ee0cff16ddb68a8a8878
Alex Everett <aeverett4746@gmail.com>
2023-11-17 23:18:43 -0700

    Cleaned up repository

--- conservation_histograms.r
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-
-setwd('/Users/r_klabacka/OneDrive - Utah Tech University/KLab/Research/BAM_comparative_genetics')
-
-dat <- read.csv('test_output.csv')
-
-ggplot(dat, aes(x=Position, y=Proportion)) + geom_bar(stat="identity")
-
