8fc6d14651d999a94f27b53ac8a7316da11b9363
Dantebob <122409098+Dantebob@users.noreply.github.com>
2023-11-28 16:16:58 -0700

    Update e_coli_farm.py
    simplified script. hope I didn't break anything

--- e_coli_farm.py
+++ e_coli_farm.py
@@ -35,25 +35,17 @@ def farm_data(infile, target_species):
     # Searches through FASTA file for target species
     for i, record in enumerate(records):
         if target_species in record.id:
-            target_species_index = i
-            break
+            target_sequence = records[i].seq
+        new_seq = ""
 
-    # Writes new farmed sequences based on target species
-    if target_species_index != None:
-        target_sequence = records[target_species_index].seq
-
-        #
-        for record in records:
-            new_seq = ""
-
-            # Compares position in target sequence to current sequence
-            for a, b in zip(target_sequence, record.seq):
-                if a != "-":
-                    new_seq += b
-            new_record = record
-            new_record.seq = new_seq
-            new_records.append(new_record)
-    return new_records
+        # Compares position in target sequence to current sequence
+        for a, b in zip(target_sequence, record.seq):
+            if a != "-":
+                new_seq += b
+        new_record = record
+        new_record.seq = new_seq
+        new_records.append(new_record)
+return new_records
 
 def generate_filename(infile):
 
