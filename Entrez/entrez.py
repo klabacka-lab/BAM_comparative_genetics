e00cb2e47420c747d116241ad57872df4aa40e74
David-Bean <146380480+David-Bean@users.noreply.github.com>
2023-12-03 14:31:02 -0700

    Adding Entrez python file
    Searches genbank for a speciefied gene across an entire list of bacteria, saves results to FASTA

--- Entrez/entrez.py
+++ Entrez/entrez.py
@@ -29,61 +29,109 @@ def get_sequences(organism_name,gene_name):
 		print(sequence)
 
 		records[description] = sequence
-
 	return records
 
 
-# Gets the first sequence and description for a given organism
-def get_sequence(organism_name,gene_name):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+database = 'protein'
+def get_record(organism_name,gene_name):
 	search_query = f'{organism_name}[Organism] AND {gene_name}[Gene]'
-	handle = Entrez.esearch(db ='protein',term = search_query)
+	handle = Entrez.esearch(db = database,term = search_query)
 	record = Entrez.read(handle)
 
+	return record
 
-
-	if len(record["IdList"]) != 0:
-		ident = record["IdList"][0]
-	else:
-		print(f"record['IdList'][0] out of range error:{organism_name}")
-		return None
-
-	handle = Entrez.efetch(db = 'protein', id = ident, rettype='gb',retmode='text')
+def get_genbank_record(i):
+	handle = Entrez.efetch(db = database, id = i, rettype='gb',retmode='text')
 	genbank_record = SeqIO.read(handle,'genbank')
-	description = genbank_record.description+f' id:{ident}'
 
-	index = 0
-	cds_feature = None
+	return genbank_record
 
-# id from id list for given may not have cds feature. If not I want to try again with a different id. May need to execute get_sequence from another function
-# and pass id into it
 
-	for feature in genbank_record.features:
+
+def find_sequence(record):
+
+	id_cds = None
+	idlist = record['IdList']
+	print(f'Len IdList = {len(idlist)}')
+
+	for i in idlist:
+		genbank_record = get_genbank_record(i)
+		for feature in genbank_record.features:
 			if feature.type == 'CDS':
-				cds_feature = feature
+				id_cds = i
+				description = genbank_record.description+f' id:{i}'
+				sequence = feature.location.extract(genbank_record.seq)
 				break
-	if cds_feature:
-		sequence = cds_feature.location.extract(genbank_record.seq)
-		# print(description,sequence)
-	else:
 
-		print(f'cds_feature error: {organism_name}')
-		return None
+	if id_cds == None:
+		print(f'CDS not found in {genbank_record.description} genbank record')
+	
+	return [genbank_record.description,str(genbank_record)]
 
-	return (description,str(sequence))
+
 
 
 fc = fc.Fasta_cross()
 fc.get_names(read_file = 'enterobacterales_short.fa',fasta_type = 'alex')
-
 bacteria_names = fc.bacteria_names
 gene_name = 'BamA'
-records = [get_sequence(organism_name = bacteria_name,gene_name = gene_name) for bacteria_name in bacteria_names]
-records = [record for record in records if record != None]
-print(records)
 
-with open('results.fasta','w') as f:
-	for record in records:
-		f.write(f'>{record[0]}\n{record[1]}\n')
+for name in bacteria_names:
+	record = get_record(organism_name = name, gene_name = gene_name)
+	sequence = find_sequence(record)
+	for thing in sequence:
+		print(thing+'\n\n')
+
+
+
+
+
+
+
+
+
+
+
+# with open('results.fasta','w') as f:
+# 	for record in records:
+# 		f.write(f'>{record[0]}\n{record[1]}\n')
 
 
 # Only getting less than half back...
+
+'''
+	for i in idlist:
+		if pull_sequence()
+
+		for feature in genbank_record.features:
+			if feature.type == 'CDS':
+				cds_feature = feature
+				break
+'''
