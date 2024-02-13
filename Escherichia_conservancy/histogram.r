206eaa777d97bee7e347431f4a869a6d2bef98fb
David-Bean <david.w.bean98@gmail.com>
2024-02-13 16:10:40 -0700

    Updated histogram R script

--- Escherichia_conservancy/histogram.r
+++ Escherichia_conservancy/histogram.r
@@ -5,12 +5,14 @@ stats <- read.csv("Gene_Proportions.csv")
 meanProp <- stats$Mean.Prop
 medProp <-stats$Med.Prop
 
-prop_of_interest = 0.99
+prop_of_interest = 0.9991
 
 hist_plot <-ggplot(stats,aes(x=Mean.Prop)) +
-  geom_histogram(aes(fill = Mean.Prop > .995),
-                 binwidth = 0.002) +
+  geom_histogram(aes(fill = Mean.Prop > .9815),
+                 binwidth = 0.0015) +
   geom_vline(aes(xintercept=prop_of_interest))
 
 hist_plot
 
+n <- nrow(subset(stats, Mean.Prop > 0.9815))
+prop_greater <- n / nrow(stats)
