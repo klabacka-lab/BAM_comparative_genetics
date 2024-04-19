f2c495f183157162f09fa70dc28a19eb80ac7009
David-Bean <david.w.bean98@gmail.com>
2024-04-19 09:03:25 -0600

    Added proportion to protein labels on histogram.r

--- Escherichia_conservancy/histogram.r
+++ Escherichia_conservancy/histogram.r
@@ -30,9 +30,9 @@ hist_plot <-ggplot(stats,aes(x=Mean.Prop)) +
   
   # Labeling vertical lines
   
-  geom_text(aes(x=bamA_proportion-label_h_offset,y = 100, angle = label_angle, label = "BamA")) +
-  geom_text(aes(x=tamA_proportion-label_h_offset,y = 110, angle = label_angle, label = "TamA")) +
-  geom_text(aes(x=secY_proportion-label_h_offset,y = 120, angle = label_angle, label = "SecY")) 
+  geom_text(aes(x=bamA_proportion-label_h_offset,y = 100, angle = label_angle, label = "BamA: 0.9815")) +
+  geom_text(aes(x=tamA_proportion-label_h_offset,y = 110, angle = label_angle, label = "TamA: 0.9889")) +
+  geom_text(aes(x=secY_proportion-label_h_offset,y = 120, angle = label_angle, label = "SecY: 0.9991")) 
   
 #Make the whole plot grey, 3 labeled black lines
   
