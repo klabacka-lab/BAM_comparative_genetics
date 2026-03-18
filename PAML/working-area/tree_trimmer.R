9921ef49b64a5a69e5169b4b3550fa14265a4140
Gavin Smith <mainuser@Gavins-MacBook-Air-362.local>
2026-03-18 15:51:24 -0600

    Ran first pruning, have not verified if successful

--- PAML/working-area/tree_trimmer.R
+++ PAML/working-area/tree_trimmer.R
@@ -2,3 +2,7 @@ library(tidyverse)
 library(ape)
 
 ## need to prune off a few branches, this is the file for 
+tree = read.tree("PAML/working-area/unroot-tree.tree")
+newtree = drop.tip(tree, tip=c("Hafnia_alvei","Rouxiella_chamberiensis","Erwinia_pyrifoliae")) 
+print(newtree)
+write.tree(newtree, file = "pruned_tree.txt")
